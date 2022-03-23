bd7d2bfaa3d647118049d0380334cd730a780e67
kristiayaya <kristiayaya08@gmail.com>
2022-03-23 21:24:20 +0700

    form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,17 +15,21 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="{{asset('js/app.js') }}"></script>
-    <title>Hello, world!</title>
+    <title>home</title>
     <link rel="stylesheet" href="{{asset('css/carousel.css') }}">
     <link rel="stylesheet" href="{{asset('css/carousel2.css') }}">
     <link rel="stylesheet" href="{{asset('css/app.css') }}">
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     
+    <script src="{{asset('js/app.js') }}"></script>
 
     <!-- Bootstrap core CSS -->
-<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <style>
+    .rata{
+      text-align: justify;
+  text-justify: inter-word;
+    }
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -42,10 +46,8 @@
     </style>
 
     
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
   </head>
-  <body>
+  <body > 
     
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -60,10 +62,10 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Kamar</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link disabled">Fasilitas</a>
           </li>
         </ul>
         <form class="d-flex">
@@ -86,17 +88,16 @@
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga5.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga2.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
         
         <div class="container">
             <div class="carousel-caption text-start">
                 <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
             </div>
         </div>
-    </div>
+   </div>
     <div class="carousel-item">
-        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga2.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-      
+        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga1.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
         <div class="container">
           <div class="carousel-caption">
@@ -104,7 +105,7 @@
         </div>
       </div>
       <div class="carousel-item">
-        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga1.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+        <img class="bd-placeholder-img" width="100%" height="100%" src="{{url('/Gambar/ga5.jpg')}}" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
 
         <div class="container">
           <div class="carousel-caption text-end">
@@ -127,9 +128,40 @@
     </button>
   </div>
 
+<div class="p-5">
+<p class="text-center fw-bold fs-2">Grand Vacation Hotel</p>
 
+<div class="container">
+  <div class="row">
+    <div class="col">
+    </div>
+    <div class="col">
+    <div class="card" style="width: 70rem;">
+  <div class="card-body">
+    <p class="card-text rata justify-text-center">Welcome to Grand Vacation Hotel. A hotel with a comfortable atmosphere and suitable for holidays. enjoy time with family, relatives, friends and certain occasions. There are various facilities, both room facilities and general hotel facilities that complement comfort and satisfaction. Let's enjoy your comfortable rest with Grand Vacation Hotel.</p>
+  </div>
+</div>
  
-
+    </div>
+    <div class="col">
+  
+    </div>
+  </div>
+</div>
+</div>
+<div class="row g-3 align-items-center">
+  <div class="col-auto">
+    <label for="inputPassword6" class="col-form-label">Password</label>
+  </div>
+  <div class="col-auto">
+    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+  </div>
+  <div class="col-auto">
+    <span id="passwordHelpInline" class="form-text">
+      Must be 8-20 characters long.
+    </span>
+  </div>
+</div>
 
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
